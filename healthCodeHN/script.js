c0e082daf65c70cfd309edf9813b1133c51b2320
maoring <maoring21@gmail.com>
2022-10-02 14:29:46 +0800

    chore: debug

--- healthCodeHN/script.js
+++ healthCodeHN/script.js
@@ -2,7 +2,7 @@ const $ = new Env('湖南健康码');
 const body = JSON.parse($response.body);
 // 获取前一天时间并格式化 YYYY-MM-DD HH:mm
 const date = new Date(new Date().getTime() - 24 * 60 * 60 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '');
-
+console.log("body=>>>>",body);
 body.data[0]['SBRQ'] = date
 
 $done({body: JSON.stringify(body)});
